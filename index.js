37cfc559a7961cf17fab81f6e0c4c85dce610543
anotherMichaelMaier <117644039+anotherMichaelMaier@users.noreply.github.com>
2022-12-07 15:41:32 +0100

    log first pokemon name

--- index.js
+++ index.js
@@ -3,17 +3,19 @@ console.clear();
 const url = "https://pokeapi.co/api/v2/pokemon";
 
 async function getPokemon() {
-    try {
-        const response = await fetch(url);
-        if (response.ok) {
-            console.log("Fetch is OK!");
-            const pokemonData = await response.json();
-            console.log(pokemonData.results);
-        } else {
-            console.log("Fetch not OK!");
-        }
-    } catch(error) {
-        console.error(error);
+  try {
+    const response = await fetch(url);
+    if (response.ok) {
+      console.log("Fetch is OK!");
+      const pokemonData = await response.json();
+      console.log(pokemonData.results);
+      const pokemonList = pokemonData.results;
+      console.log(pokemonList[0].name);
+    } else {
+      console.log("Fetch not OK!");
     }
+  } catch (error) {
+    console.error(error);
+  }
 }
 getPokemon();
